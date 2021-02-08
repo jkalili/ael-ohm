6a68987a8c895b862663249562e8fe020ef31794
Jason Kalili <45611022+jkalili@users.noreply.github.com>
2021-02-07 23:31:31 -0800

    finished problem 1

--- src/parser.js
+++ src/parser.js
@@ -1,33 +1,39 @@
 // Parser
-//
 // Exports a single function called parse which accepts the source code
 // as a string and returns the AST.
 
 import ohm from "ohm-js"
 import * as ast from "./ast.js"
 
-const aelGrammar = ohm.grammar(String.raw`Ael {
-  Program   = Statement+
-  Statement = let id "=" Exp                  --variable
-            | id "=" Exp                      --assign
-            | print Exp                       --print
-  Exp       = Exp ("+" | "-") Term            --binary
-            | Term
-  Term      = Term ("*"| "/") Factor          --binary
-            | Factor
-  Factor    = id
-            | num
-            | "(" Exp ")"                     --parens
-            | ("-" | abs | sqrt) Factor       --unary
-  num       = digit+ ("." digit+)?
-  let       = "let" ~alnum
-  print     = "print" ~alnum
-  abs       = "abs" ~alnum
-  sqrt      = "sqrt" ~alnum
-  keyword   = let | print | abs | sqrt
-  id        = ~keyword letter alnum*
-  space    += "//" (~"\n" any)* ("\n" | end)  --comment
-}`)
+const aelGrammar = ohm.grammar(String.raw`
+  Ael {
+    Program   = Statement+  
+    Statement = let id "=" Equal                  --variable
+              | id "=" Equal                      --assign
+              | print Equal                       --print    
+    Equal     = Exp ("==") Exp                    --equal
+              | Exp
+    Exp       = Exp ("+" | "-") Term              --binary
+              | Term
+    Term      = Term ("*" | "/" | "%") Factor     --binary
+              | Factor
+    Factor    = id
+              | "(" Exp ")"                       --parens
+              | ("-" | abs | sqrt) Exp            --unary
+              | Exponent  
+    Exponent      = num ("**") Exponent           --exponent
+              | num    
+
+    num       = digit+ ("." digit+)?
+    let       = "let" ~alnum
+    print     = "print" ~alnum
+    abs       = "abs" ~alnum
+    sqrt      = "sqrt" ~alnum
+    keyword   = let | print | abs | sqrt
+    id        = ~keyword letter alnum*
+    space    += "//" (~"\n" any)* ("\n" | end)     --comment
+  }
+`)
 
 const astBuilder = aelGrammar.createSemantics().addOperation("ast", {
   Program(body) {
